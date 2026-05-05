8782a0bd6f59fd3b1f73157c5c4ee83f5df840e0
ssalmao <salome.lohner@stud.fhgr.ch>
2026-05-05 13:55:28 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("blub");
+async function loadData() {
+    const url = 'https://currentuvindex.com/api/v1/uvi?latitude=40.6943&longitude=-73.9249'; // mit korrekter API-URL ersetzen
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+const data = await loadData();
+console.log(data); // gibt die Daten der API oder false in der Konsole ausconsole.log('hoi')
